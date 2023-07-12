c543b9e3d25d1155172297f0bb02775d2706e736
Gabriel Martínez <martinegabriel47@gmail.com>
2023-07-12 16:33:42 -0500

    Componente loading

---
+++ src/components/loading/loading.jsx
@@ -0,0 +1,11 @@
+import './loading.css'
+
+function Loading() {
+    return (
+        <div className="loading">
+            <box-icon name='loader-circle' animation='spin' color='#00ffff' size="90px" ></box-icon>
+        </div>
+    )
+}
+
+export default Loading
